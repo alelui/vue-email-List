d9a66d4ff2ac81d2b6248ed3e395e807583fc78c
Alessandro <luisialessandro95@gmail.com>
2021-12-12 16:19:42 +0100

    esercizio

--- js/myscript.js
+++ js/myscript.js
@@ -2,12 +2,26 @@
 const app = new Vue({
     el : '#root',
     data:{
-
+        emails: []
     },
     methods:{
 
     },
     created(){
 
+        for(let i = 0; i < 10; i++){
+            // Make a request for a user with a given ID
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                //console.log(response.data.response);
+                this.emails.push(response.data.response);
+                // console.log(this.emails);
+            })
+
+            .catch(function (error) {
+                console.log(error);
+            })
+        }
     }
+
 })
